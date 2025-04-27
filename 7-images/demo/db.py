2b0a7bf4bbf026f11437d4695d140c99e0585ff0
mmmekonen <maelat.mekonen@gmail.com>
2025-04-27 19:42:15 -0400

    fixed url

--- 7-images/demo/db.py
+++ 7-images/demo/db.py
@@ -16,7 +16,7 @@ db = SQLAlchemy()
 EXTENSIONS = ["png", "gif", "jpg", "jpeg"]
 BASE_DIR = os.getcwd()
 S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
-S3_BASE_URL = f"https://{S3_BUCKET_NAME}.s3.us-east-1"
+S3_BASE_URL = f"https://{S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com"
 
 
 class Asset(db.Model):
